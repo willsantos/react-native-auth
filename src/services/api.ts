afca780f0e83763eadeb5aa28456ed41b77ac84c
Wilson Santos <contato@wilsonsantos.net>
2020-10-18 14:04:23 -0300

    Add fake api service

---
+++ src/services/api.ts
@@ -0,0 +1,13 @@
+export default function SignIn() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        token: 'askdjsakdjjasbfjn21j1k3k12jiujdsaf89sad',
+        user: {
+          name: 'Wilson Santos',
+          email: 'will@example.com',
+        },
+      });
+    }, 1500);
+  });
+}
