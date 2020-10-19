a4400e8a2e605e4510748707d5f873ae61296be7
Wilson Santos <contato@wilsonsantos.net>
2020-10-19 15:19:34 -0300

    Add set and get credentials in asyncstorage

--- src/contexts/auth.tsx
+++ src/contexts/auth.tsx
@@ -1,4 +1,5 @@
-import React, {createContext, useState} from 'react';
+import React, {createContext, useState, useEffect} from 'react';
+import AsyncStorage from '@react-native-community/async-storage';
 import * as auth from '../services/api';
 
 interface AuthContextData {
@@ -13,14 +14,31 @@ const authContext = createContext<AuthContextData>({} as AuthContextData);
 export const AuthProvider: React.FC = ({children}) => {
   const [user, setUser] = useState<object | null>(null);
 
+  useEffect(() => {
+    async function loadStorageData() {
+      const storageUser = await AsyncStorage.getItem('@WILLauth:user');
+      const storageToken = await AsyncStorage.getItem('@WILLauth:token');
+
+      if (storageUser && storageToken) {
+        setUser(JSON.parse(storageUser));
+      }
+    }
+
+    loadStorageData();
+  }, []);
+
   async function signIn() {
     const response = await auth.signIn();
 
     setUser(response.user);
+    await AsyncStorage.setItem('@WILLauth:user', JSON.stringify(response.user));
+    await AsyncStorage.setItem('@WILLauth:token', response.token);
   }
 
-  async function signOut() {
-    setUser(null);
+  function signOut() {
+    AsyncStorage.clear().then(() => {
+      setUser(null);
+    });
   }
   return (
     <authContext.Provider value={{signed: !!user, user, signIn, signOut}}>
